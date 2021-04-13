3c065cf371079a8adff3d4b271046ebb04888df3
Louis <louis.barre.info@gmail.com>
2021-04-13 11:17:37 +0200

    remove chest entity add chest content to userRessource

--- config/bundles.php
+++ config/bundles.php
@@ -12,6 +12,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['dev' => true, 'test'=> true],
     Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
 ];
